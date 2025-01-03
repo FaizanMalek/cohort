dc5dc0a1c3eca43573c71ba665e4bab0e5cd77bb
FaizanMalek <fmalek01@mynbcc.ca>
2025-01-03 02:57:17 -0400

    zod library for Input validation

--- week3/middlewares/InputValGlobalCatches.js
+++ week3/middlewares/InputValGlobalCatches.js
@@ -1,12 +1,17 @@
 const express= require ("express");
 const app=express();
+const zod = require("zod");
+
+const schema = zod.array(zod.number());
 
 app.use(express.json());
 
 app.post("/health-checkup",(req,res)=>{
     const kidneys=req.body.kidneys;
-    const kidneyLength=kidneys.length;
-    res.send("your have kidneys  = " + kidneyLength);
+    const response= schema.safeParse(kidneys);
+    res.send(response);
+    // const kidneyLength=kidneys.length;
+    // res.send("your have kidneys  = " + kidneyLength);
 })
 
 app.use((err,req,res,next)=>
