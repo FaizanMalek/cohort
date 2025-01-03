ac861f9dc7a066a3d8302181dcdd1dabb290b003
FaizanMalek <fmalek01@mynbcc.ca>
2025-01-03 02:09:27 -0400

    checking headers:user/pass using middleware

--- week3/middlewares/middlewares.js
+++ week3/middlewares/middlewares.js
@@ -1,7 +1,41 @@
 const express= require("express");
 const app = express();
 
-app.get
+function kidneyMiddleware(req,res,next) {
+    const kidneyId = req.query.kidneyId;
+    if(kidneyId!=1 && kidneyId!=2)
+    {
+        res.status(404).json({msg: "something wrong with your kidney inputs"});
+        return;
+    }
+    next();
+}
+
+function userMiddleware(req,res,next) {
+    const user = req.headers.user;
+    const pass = req.headers.pass;
+    if(user!="user" || pass!="pass")
+    {
+        res.status(404).json({msg:" something wrong with your user pass"});
+        return;
+    }
+    next();
+}
+
+app.use(userMiddleware);
+app.use(express.json());
+
+app.get("/health-checkup",kidneyMiddleware,(req,res)=>{
+    res.send("you are healthy");
+})
+
+app.get("/kidney-check", kidneyMiddleware,(req,res)=>{
+    res.send("your kidneys are healthy");
+})
+
+app.get("/heart-check", (req,res)=>{
+    res.send("your heart is healthy");
+})
 
 
 app.listen(3000);
