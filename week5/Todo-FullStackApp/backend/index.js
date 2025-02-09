b8386ea059861a15000c15be7d42b1f123088d87
FaizanMalek <fmalek01@mynbcc.ca>
2025-02-09 18:45:50 -0400

    added validation in post and put routes

--- week5/Todo-FullStackApp/backend/index.js
+++ week5/Todo-FullStackApp/backend/index.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json())
+const {createTodo, updateTodo} = require("./types.js")
 
 
 // body {  
@@ -12,7 +13,15 @@ app.use(express.json())
 // } u would expect this from the user and so u need to validate this using Zod.
 //  Make a types.js file and add Zod logic
 app.post('/todo', (req,res)=> {
-
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg : "You sent the wrong inputs"
+        })
+        return;
+    }
+    //put it in mongodb
 })
 
 app.get('/todos', (req,res) => {
@@ -20,7 +29,14 @@ app.get('/todos', (req,res) => {
 })
 
 app.put("/completed", (req,res) => {
-
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(completeId);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg : "You sent the wrong inputs"
+        })
+        return;
+    }
 })
 
 
